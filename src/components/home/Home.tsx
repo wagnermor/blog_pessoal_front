f41214a88cbeae914d66c42ed769f81978ae9a12
WagnerMor <wagnermor@outlook.com>
2023-02-27 23:38:51 -0300

    Add: Create Home component

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi earum obcaecati, inventore repellendus veritatis quo sequi facere ipsam! Consequatur aperiam, delectus voluptatem quam repellat ducimus quibusdam suscipit debitis unde repudiandae.</p>
+    </>
+  )
+}
+
+export default Home;
