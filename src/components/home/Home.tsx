b434992e1e154913aa51e1e7e10434031b137d63
WagnerMor <wagnermor@outlook.com>
2023-02-27 23:57:10 -0300

    Fix: Home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
 import './home.css';
 
-function Home() {
+export default function Home() {
   return (
     <>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi earum obcaecati, inventore repellendus veritatis quo sequi facere ipsam! Consequatur aperiam, delectus voluptatem quam repellat ducimus quibusdam suscipit debitis unde repudiandae.</p>
     </>
   )
 }
-
-export default Home;
