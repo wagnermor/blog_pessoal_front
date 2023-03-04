888e14eff85fa494c06c74d7d232181459ebdddd
WagnerMor <wagnermor@outlook.com>
2023-03-04 13:32:39 -0300

    Fix: Add Login route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './App.css';
 import Navbar from './components/statics/navbar/Navbar';
 import Footer from './components/statics/footer/Footer';
 import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Teste from './pages/teste/Teste';
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/teste" element={<Teste />} />
         </Routes>
         <Footer />
       </Router>
