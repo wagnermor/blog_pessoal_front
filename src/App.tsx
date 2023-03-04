12e9f54cfedc82aa9f4312b971f86b105c1c9508
WagnerMor <wagnermor@outlook.com>
2023-03-04 12:40:13 -0300

    Fix: Add routes in /App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 import './App.css';
 
-import Home from './components/Home/home';
 import Navbar from './components/statics/navbar/Navbar';
 import Footer from './components/statics/footer/Footer';
+import Home from './pages/home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
