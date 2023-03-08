21922ded45a4a5d218b5ef1374eda3f8805bf422
WagnerMor <wagnermor@outlook.com>
2023-03-08 19:56:00 -0300

    Fix: Home.tsx and Home.css

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,14 +1,55 @@
+import { Typography, Grid, Button } from '@material-ui/core';
+import { Box } from '@mui/material';
 import './Home.css';
-import note from '../../assets/img/note.jpg';
 
-function Home() {
+export default function Home() {
   return (
-    <div className='homeMain'>
-      <div className="filha">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi earum obcaecati, inventore repellendus veritatis quo sequi facere ipsam! Consequatur aperiam, delectus voluptatem quam repellat ducimus quibusdam suscipit debitis unde repudiandae.</p>
-        <img className='img_note' src={note} alt="Notebook" />
-      </div>
-    </div>
+    <Grid className='boxMain'
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      
+      <Grid alignItems="center" item xs={6}>
+        <Box paddingX={20}>
+          <Typography className='title_home'
+            variant="h4"
+            gutterBottom
+            color="textPrimary"
+            component="h3"
+            align="center"
+          >
+            Seja bem vindo(a)!
+          </Typography>
+          <Typography className='title_home'
+            variant="h5"
+            gutterBottom
+            color="textPrimary"
+            component="h5"
+            align="center"
+          >
+            expresse aqui os seus pensamentos e opiniões!
+          </Typography>
+        </Box>
+        <Box display="flex" justifyContent="center">
+          <Box marginRight={1}></Box>
+          <Button className='btn_ver_postagem'
+            variant="outlined"
+          >
+            Ver Postagens
+          </Button>
+        </Box>
+      </Grid>
+      <Grid item xs={6}>
+        <img
+          src="https://i.imgur.com/H88yIo2.png"
+          alt=""
+          width="500px"
+          height="500px"
+        />
+      </Grid>
+      <Grid xs={12} style={{ backgroundColor: 'white' }}>Tab postagem </Grid>
+    </Grid>
   );
 }
-export default Home;
