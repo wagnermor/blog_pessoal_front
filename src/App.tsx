176c57084d620af742c88e1a8fac21bbfb666317
WagnerMor <wagnermor@outlook.com>
2023-03-09 13:29:13 -0300

    Fix: add cadastro-usuario route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Navbar from './components/statics/navbar/Navbar';
 import Footer from './components/statics/footer/Footer';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
+import CadastroUsuario from './pages/cadastroUsuario/CadastroUsuario';
 import TesteGrid from './pages/teste/TesteGrid';
 import TesteFwlexBox from './pages/teste/TesteFlexBox';
 
@@ -19,6 +20,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/cadastro-usuario" element={<CadastroUsuario />} />
             <Route path="/teste-grid" element={<TesteGrid />} />
             <Route path="/teste-flexbox" element={<TesteFwlexBox />} />
           </Routes>
