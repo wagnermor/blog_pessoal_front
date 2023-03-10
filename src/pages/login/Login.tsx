a9477a86fb05e84ef6ab2fc956100c28187d99e4
WagnerMor <wagnermor@outlook.com>
2023-03-09 23:08:03 -0300

    Fix:Add localstorage dependenci

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,23 +1,62 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect, useState } from 'react'
 import { Button, Grid, Typography, TextField } from '@material-ui/core'
+
+import useLocalStorage from 'react-use-localstorage';
+import UserLogin from '../../models/UserLogin';
+import { login } from '../../services/Service';
 import { Box } from '@mui/material'
 
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Login() {
+  let navigate = useNavigate()
+  const [token, setToken] = useLocalStorage('token')
+  const [userLogin, setUserLogin] = useState<UserLogin>(
+    {
+      id: 0,
+      nome: '',
+      usuario:'',
+      senha:'',
+      foto:'',
+      token:''
+    }
+  )
+
+  function updateModel(e: ChangeEvent<HTMLInputElement>) {
+    setUserLogin({
+      ...userLogin,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault()
+    try{
+      await login(`/usuarios/logar`, userLogin, setToken)
+      alert(`Usuário logado com sucesso!`)
+    }catch(error){
+      alert(`Dados do usuário inconsistentes. Erro ao logar!`)
+    }
+  }
+
+  useEffect(() => {
+    if(token !== '')
+      navigate('/home')
+  }, [token])
+  
   return (
     <Grid container direction="row" justifyContent="center" alignItems="center">
       <Grid alignItems="center" xs={6}>
         <Box paddingX={20}>
-          <form>
+          <form onSubmit={onSubmit}>
             <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ fontWeight: 'bold' }}>
               Entrar
             </Typography>
             <TextField
               className='input_form'
-              // value={userLogin.usuar}
-              // onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)}
+              value={userLogin.usuario}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)}
               id="usuario"
               label="usuário"
               variant="outlined"
@@ -26,8 +65,8 @@ export default function Login() {
               fullWidth />
             <TextField
               className='input_form'
-              // value={userLogin.senha}
-              // onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)}
+              value={userLogin.senha}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => updateModel(e)}
               id="senha"
               label="senha"
               variant="outlined"
@@ -36,11 +75,9 @@ export default function Login() {
               type="password"
               fullWidth />
             <Box marginTop={2} textAlign="center">
-              
-                <Button className='btn_logar' type="submit" variant="contained">
-                  Logar
-                </Button>
-              
+              <Button className='btn_logar' type="submit" variant="contained">
+                Logar
+              </Button>
             </Box>
           </form>
           <Box display="flex" justifyContent="center" marginTop={2}>
