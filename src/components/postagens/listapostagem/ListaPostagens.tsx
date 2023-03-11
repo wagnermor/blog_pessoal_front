6d15a88146e633bc65b7ceb3a4c8808801191836
WagnerMor <wagnermor@outlook.com>
2023-03-11 12:13:44 -0300

    Feat: Add ListaPostagens.css and ListaPostagem.tsx

---
+++ src/components/postagens/listapostagem/ListaPostagens.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import "./ListaPostagens.css";
+import { busca } from "../../../services/Service";
+import useLocalStorage from "react-use-localstorage";
+import { useNavigate, Link } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Typography, Button, Card, CardActions, CardContent } from "@material-ui/core";
+import Postagem from "../../../models/Postagem";
+
+export default function ListaPostagens() {
+  const [posts, setPosts] = useState<Postagem[]>([]);
+  const [token, setToken] = useLocalStorage("token");
+  let navigate = useNavigate();
+
+  async function getPost() {
+    await busca("/postagens", setPosts, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  useEffect(() => {
+    if (token == "") {
+      alert("Você precisa estar logado");
+      navigate("/login");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    getPost();
+  }, [posts.length]);
+
+  return (
+    <>
+      {posts.map((post) => (
+        <Box m={2}>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography color="textSecondary" gutterBottom>
+                Postagens
+              </Typography>
+              <Typography variant="h5" component="h2">
+                {post.titulo}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {post.texto}
+              </Typography>
+              <Typography variant="body2" component="p">
+                {post.tema?.descricao}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Box display="flex" justifyContent="center" mb={1.5}>
+                <Link
+                  to={`/formularioPostagem/${post.id}`}
+                  className="text-decorator-none"
+                >
+                  <Box mx={1}>
+                    <Button
+                      variant="contained"
+                      className="marginLeft"
+                      size="small"
+                      color="primary"
+                    >
+                      atualizar
+                    </Button>
+                  </Box>
+                </Link>
+                <Link
+                  to={`/deletarPostagem/${post.id}`}
+                  className="text-decorator-none"
+                >
+                  <Box mx={1}>
+                    <Button variant="contained" size="small" color="secondary">
+                      deletar
+                    </Button>
+                  </Box>
+                </Link>
+              </Box>
+            </CardActions>
+          </Card>
+        </Box>
+      ))}
+    </>
+  );
+}
