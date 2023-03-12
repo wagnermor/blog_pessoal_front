732895ef7c6cd3eb71a33e07bd506625768880b1
WagnerMor <wagnermor@outlook.com>
2023-03-12 20:36:22 -0300

    Feat: Add CadastroPostagem, DeletarPostagem and ModalPostagem

---
+++ src/components/postagens/deletarPostagem/DeletarPostagem.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from "react";
+import {
+  Typography,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+} from "@material-ui/core";
+import { Box } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
+import { buscaId, deleteId } from "../../../services/Service";
+import "./DeletarPostagem.css";
+import Postagem from "../../../models/Postagem";
+
+export default function DeletarPostagens() {
+  let navigate = useNavigate();
+  const { id } = useParams<{ id: string }>();
+  const [token, setToken] = useLocalStorage("token");
+  const [post, setPosts] = useState<Postagem>();
+
+  useEffect(() => {
+    if (token == "") {
+      alert("Você precisa estar logado");
+      navigate("/login");
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (id !== undefined) {
+      findById(id);
+    }
+  }, [id]);
+
+  async function findById(id: string) {
+    buscaId(`/postagens/${id}`, setPosts, {
+      headers: {
+        Authorization: token,
+      },
+    });
+  }
+
+  function sim() {
+    navigate("/postagens");
+    deleteId(`/postagens/${id}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    alert("Postagem deletada com sucesso");
+  }
+
+  function nao() {
+    navigate("/postagens");
+  }
+
+  return (
+    <>
+      <Box m={2}>
+        <Card variant="outlined">
+          <CardContent>
+            <Box justifyContent="center">
+              <Typography color="textSecondary" gutterBottom>
+                Deseja deletar a Postagem:
+              </Typography>
+              <Typography color="textSecondary">{post?.titulo}</Typography>
+            </Box>
+          </CardContent>
+          <CardActions>
+            <Box display="flex" justifyContent="start" ml={1.0} mb={2}>
+              <Box mx={2}>
+                <Button
+                  onClick={sim}
+                  variant="contained"
+                  className="marginLeft"
+                  size="large"
+                  color="primary"
+                >
+                  Sim
+                </Button>
+              </Box>
+              <Box>
+                <Button
+                  onClick={nao}
+                  variant="contained"
+                  size="large"
+                  color="secondary"
+                >
+                  Não
+                </Button>
+              </Box>
+            </Box>
+          </CardActions>
+        </Card>
+      </Box>
+    </>
+  );
+}
