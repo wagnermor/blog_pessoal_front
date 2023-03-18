21e410cdf2430078c469488aea34fa0bbeb00b8a
WagnerMor <wagnermor@outlook.com>
2023-03-18 12:22:28 -0300

    Fix: Add toast.info in Navbar.tsx

--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
 
   function goLogout() {
     dispatch(addToken(''));
-    toast.success("Usuário deslogado", {
+    toast.info("Usuário deslogado", {
       theme:'colored'
     })
     navigate('/login')
